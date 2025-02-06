0f56fe451600201eb95a43a9916245d6764876d9
Victor Vili <victorlunaxavierl@gmail.com>
2025-02-06 18:04:25 -0300

    initial build

---
+++ src/main/java/com/hobbyFinder/hubby/HubbyApplication.java
@@ -0,0 +1,13 @@
+package com.hobbyFinder.hubby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubbyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hobbyFinder/hubby/HubbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobbyFinder.hubby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
