488608c51fd015c1115ecd7a2fe6b9bdd58ed89c
Rodrigo Félix <rodrigo.nascimento@ccc.ufcg.edu.br>
2025-02-27 23:41:46 -0300

    base

--- src/main/java/com/hobbyFinder/hubby/services/ServicesImpl/UserService.java
+++ src/main/java/com/hobbyFinder/hubby/services/ServicesImpl/UserService.java
@@ -1,24 +1,39 @@
 package com.hobbyFinder.hubby.services.ServicesImpl;
 
-import com.hobbyFinder.hubby.models.dto.user.UserDTO;
-import com.hobbyFinder.hubby.repositories.UserRepository;
-import com.hobbyFinder.hubby.services.IServices.UserInterface;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
+import com.hobbyFinder.hubby.models.dto.user.UserDTO;
+import com.hobbyFinder.hubby.repositories.UserRepository;
+import com.hobbyFinder.hubby.services.IServices.UserInterface;
 
 @Service
 public class UserService implements UserInterface {
 
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-    @Override
-    public UserDTO getUser(UUID uuid) {
-        return userRepository.findById(uuid)
-                .map(user -> new UserDTO(user.getEmail(), user.getUsername(), user.getRole()))
-                .orElseThrow(() -> new UsernameNotFoundException("User não encontrado."));
-    }
+  @Autowired
+  private EmailService emailService;
+
+  @Override
+  public UserDTO getUser(UUID uuid) {
+    return userRepository
+      .findById(uuid)
+      .map(user ->
+        new UserDTO(user.getEmail(), user.getUsername(), user.getRole())
+      )
+      .orElseThrow(() -> new UsernameNotFoundException("User não encontrado."));
+  }
+
+  public void enviarEmailCadastro(String email) {
+    emailService.enviarEmailTexto(
+      email,
+      "Novo usuário cadastrado",
+      "Você está recebendo um email de cadastro"
+    );
+  }
 }
