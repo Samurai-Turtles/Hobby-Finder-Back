c6b6245b79d1628b7ff68fbb39828836f75600a9
Victor Vili <victor.vili.xavier.luna@ccc.ufcg.edu.br>
2025-03-01 11:14:50 -0300

    feat: delete user

--- src/main/java/com/hobbyFinder/hubby/services/ServicesImpl/UserService.java
+++ src/main/java/com/hobbyFinder/hubby/services/ServicesImpl/UserService.java
@@ -3,7 +3,9 @@ package com.hobbyFinder.hubby.services.ServicesImpl;
 import com.hobbyFinder.hubby.models.dto.user.UserDTO;
 import com.hobbyFinder.hubby.repositories.UserRepository;
 import com.hobbyFinder.hubby.services.IServices.UserInterface;
+import com.hobbyFinder.hubby.models.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +22,19 @@ public class UserService implements UserInterface {
         return userRepository.findById(uuid)
                 .map(user -> new UserDTO(user.getEmail(), user.getUsername(), user.getRole()))
                 .orElseThrow(() -> new UsernameNotFoundException("User não encontrado."));
+    }
+
+    @Override
+    public void deleteUser() {
+        User user = getUserLogged();
+        userRepository.delete(user);
+        userRepository.flush();
+    }
+
+    // essa função serve para pegar um usuário já logado via token jwt
+    private User getUserLogged() {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByUsername(name);
+        return user;
     }
 }
