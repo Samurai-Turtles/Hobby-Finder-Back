c3c9b91532f9b075c1e59a8a547ce32ceccd2dce
JWillianSl <jose.williansl@hotmail.com>
2025-03-02 21:51:30 -0300

    feat: adiciona anotações de validação aos parâmetros de `EventCreateDto`

--- src/main/java/com/hobbyFinder/hubby/models/dto/events/EventCreateDto.java
+++ src/main/java/com/hobbyFinder/hubby/models/dto/events/EventCreateDto.java
@@ -5,12 +5,22 @@ import java.time.LocalDateTime;
 import com.hobbyFinder.hubby.models.dto.LocalDto;
 import com.hobbyFinder.hubby.models.enums.PrivacyEnum;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record EventCreateDto(
-        String Name,
-        LocalDateTime begin,
-        LocalDateTime end,
-        LocalDto local,
-        PrivacyEnum privacy,
-        String description,
-        int MaxUserAmmount) {
+                @NotBlank String Name,
+
+                @NotNull LocalDateTime begin,
+
+                @NotNull LocalDateTime end,
+
+                @NotNull @Valid LocalDto local,
+
+                @NotNull PrivacyEnum privacy,
+
+                @NotNull String description,
+
+                int MaxUserAmmount) {
 }
