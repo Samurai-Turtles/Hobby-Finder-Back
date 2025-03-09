044ea81072c4d03feb6a50584d6b0ff9d80ff1d0
gabriel-cvt <gabriel.cavalcanti.araujo@ccc.ufcg.edu.br>
2025-03-09 17:45:42 -0300

    event tests environment

---
+++ src/test/java/com/hobbyFinder/hubby/controllerTest/EventTests/UpdateTest.java
@@ -0,0 +1,15 @@
+package com.hobbyFinder.hubby.controllerTest.EventTests;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@ActiveProfiles("test")
+@DisplayName("Testes de rota: edição de evento")
+public class UpdateTest {
+}
