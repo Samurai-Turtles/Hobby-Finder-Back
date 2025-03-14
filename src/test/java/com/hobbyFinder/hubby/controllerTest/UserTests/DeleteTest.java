162c2e452d00c3b4ae8d15b8ab58a46f77dead6d
gabriel-cvt <gabriel.cavalcanti.araujo@ccc.ufcg.edu.br>
2025-03-14 15:18:58 -0300

    test: delete users test

--- src/test/java/com/hobbyFinder/hubby/controllerTest/UserTests/DeleteTest.java
+++ src/test/java/com/hobbyFinder/hubby/controllerTest/UserTests/DeleteTest.java
@@ -1,10 +1,23 @@
 package com.hobbyFinder.hubby.controllerTest.UserTests;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hobbyFinder.hubby.controller.routes.UserRoutes;
+import com.hobbyFinder.hubby.repositories.UserRepository;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -12,4 +25,47 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 @DisplayName("Testes de rota: deleção de usuário")
 public class DeleteTest {
+
+
+    @Autowired
+    private MockMvc driver;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private UserSeeder userSeeder;
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        userSeeder = new UserSeeder(driver, objectMapper);
+        userSeeder.seedUsers();
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {}
+
+    @Transactional
+    @Test
+    @DisplayName("Usuário deletado com sucesso")
+    void testUsuarioDeletadoComSucesso() throws Exception {
+
+        String token = userSeeder.loginPrimeiroUser();
+
+        driver.perform(delete(UserRoutes.DELETE)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(status().isNoContent());
+
+        assertEquals(1, userRepository.count());
+    }
+
+    @Transactional
+    @Test
+    @DisplayName("Usuário deletado sem token")
+    void testDeletarUsuarioSemToken() throws Exception {
+        driver.perform(delete(UserRoutes.DELETE)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
 }
