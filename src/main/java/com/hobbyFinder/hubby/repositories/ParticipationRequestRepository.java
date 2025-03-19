f5b57aa2f207a0a169d78f52c8ddd214ad8c6a94
JWillianSl <jose.williansl@hotmail.com>
2025-03-18 21:51:33 -0300

    feat: adiciona um método-consulta ao repositório de solicitações Adiciona um novo método que permitirá consultar as solicitações relativas a um evento.

--- src/main/java/com/hobbyFinder/hubby/repositories/ParticipationRequestRepository.java
+++ src/main/java/com/hobbyFinder/hubby/repositories/ParticipationRequestRepository.java
@@ -2,10 +2,15 @@ package com.hobbyFinder.hubby.repositories;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.hobbyFinder.hubby.models.entities.Event;
 import com.hobbyFinder.hubby.models.entities.ParticipationRequest;
 
 public interface ParticipationRequestRepository extends JpaRepository<ParticipationRequest, UUID> {
+
+    Page<ParticipationRequest> findByEvent(Event event, Pageable pageable);
 
 }
