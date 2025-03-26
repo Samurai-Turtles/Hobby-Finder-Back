c1c65101b4b42f7a7fde451838b6ecf2d888be6c
Rodrigo Félix <rodrigo.nascimento@ccc.ufcg.edu.br>
2025-03-26 17:24:57 -0300

    emailService

---
+++ src/main/java/com/hobbyFinder/hubby/models/entities/Email.java
@@ -0,0 +1,3 @@
+package com.hobbyFinder.hubby.models.entities;
+
+public record Email(String to, String subject, String body) {}
---
+++ src/main/java/com/hobbyFinder/hubby/services/ServicesImpl/EmailService.java
@@ -0,0 +1,26 @@
+package com.hobbyFinder.hubby.services.ServicesImpl;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.hobbyFinder.hubby.models.entities.Email;
+
+@Service
+public class EmailService {
+
+  private final JavaMailSender mailSender;
+
+  public EmailService(JavaMailSender mailSender) {
+    this.mailSender = mailSender;
+  }
+
+  public void sendEmail(Email email) {
+    var message = new SimpleMailMessage();
+    message.setFrom("noreply@gmail.com");
+    message.setTo(email.to());
+    message.setSubject(email.subject());
+    message.setText(email.body());
+    mailSender.send(message);
+  }
+}
