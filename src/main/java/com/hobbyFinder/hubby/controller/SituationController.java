8f1ca607b7b1f6ea9be65669eb291ab4e9930708
Gabriel Emannuel Gama Andrade <gabriel.emannuel.gama.andrade@gmail.com>
2025-03-29 11:12:01 -0300

    fix: cors configuration

--- src/main/java/com/hobbyFinder/hubby/controller/SituationController.java
+++ src/main/java/com/hobbyFinder/hubby/controller/SituationController.java
@@ -4,7 +4,6 @@ import com.hobbyFinder.hubby.controller.routes.SituationRoutes;
 import com.hobbyFinder.hubby.models.dto.situations.SituationDto;
 import com.hobbyFinder.hubby.services.IServices.SituationInterface;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
