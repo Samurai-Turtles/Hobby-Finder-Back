596af35c54e4c1d0ebb044d3628b3e571c872279
gabriel-cvt <gabriel.cavalcanti.araujo@ccc.ufcg.edu.br>
2025-04-03 01:56:15 -0300

    fix: photo controller

--- src/main/java/com/hobbyFinder/hubby/controller/PhotoController.java
+++ src/main/java/com/hobbyFinder/hubby/controller/PhotoController.java
@@ -3,11 +3,14 @@ package com.hobbyFinder.hubby.controller;
 import com.hobbyFinder.hubby.controller.routes.PhotoRoutes;
 import com.hobbyFinder.hubby.services.ServicesImpl.PhotoService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.print.attribute.standard.Media;
 import java.util.UUID;
 
 @RestController
@@ -16,13 +19,15 @@ public class PhotoController {
 
     private final PhotoService photoService;
 
-    @PostMapping(PhotoRoutes.PHOTO_BY_USER_LOGGED)
-    public void postByUser(MultipartFile file) {
+    @PostMapping(value = PhotoRoutes.PHOTO_BY_USER_LOGGED, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void postByUser(@RequestParam("file") MultipartFile file) {
         this.photoService.uploadUserPhoto(file);
     }
 
-    @PostMapping(PhotoRoutes.PHOTO_BY_EVENT)
-    public void postByEvent(UUID id, MultipartFile file) {
+    @PostMapping(value = PhotoRoutes.PHOTO_BY_EVENT, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void postByEvent(
+            UUID id,
+            @RequestParam("file")MultipartFile file) {
         this.photoService.uploadEventPhoto(id, file);
     }
 
